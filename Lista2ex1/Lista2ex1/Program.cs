580adfb02b6727057d99be77f25a14abf27c2a29
ffrancielle <165734193+ffrancielle@users.noreply.github.com>
2024-04-29 20:07:40 -0300

    Add files via upload

---
+++ Lista2ex1/Lista2ex1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+
+            Console.Write("Digite o 1° valor: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o 2° valor: ");
+            b = int.Parse(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine("O primeiro valor é maior");
+            }
+            else
+            {
+                Console.WriteLine("O segundo valor é maior");
+            }
+
+        }
+    }
+}
