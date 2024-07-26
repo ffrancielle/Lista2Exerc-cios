c1a5a58bc8a20acb44f62645b3ee4e41a2ceb650
ffrancielle <165734193+ffrancielle@users.noreply.github.com>
2024-07-26 15:56:08 -0300

    Add files via upload

---
+++ Lista2ex4/Lista2ex4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseRetangulo;
+            double alturaRetangulo;
+            double area;
+
+            Console.Write("Digite a base do retângulo: ");
+            baseRetangulo = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a altura do retângulo: ");
+            alturaRetangulo = Convert.ToDouble(Console.ReadLine());
+
+            area = baseRetangulo * alturaRetangulo;
+
+            Console.Write("A área do retângulo é: ");
+            Console.WriteLine(area);
+
+            if (area > 100)
+            {
+                Console.WriteLine("Terreno grande");
+            }
+        }
+    }
+}
