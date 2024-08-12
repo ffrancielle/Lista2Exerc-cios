69dd41adfebaee88eeb6cdf5f27895fca13b1e0c
ffrancielle <165734193+ffrancielle@users.noreply.github.com>
2024-08-12 15:14:07 -0300

    Add files via upload

---
+++ Lista2ex10/Lista2ex10/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2ex10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double notaP1;
+            double notaP2;
+            double media;
+
+            Console.Write("Digite o valor da nota P1: ");
+            notaP1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o valor da nota P2: ");
+            notaP2 = Convert.ToDouble(Console.ReadLine());
+
+            media = (notaP1 + 2 * notaP2) / 3;
+
+            Console.WriteLine("A média do aluno é: {0}", media);
+
+            if (media >= 5)
+            {
+                Console.WriteLine("Situação do aluno: Aprovado");
+            }
+            else
+            {
+                Console.WriteLine("Situação do aluno: Reprovado");
+            }
+        }
+    }
+}
